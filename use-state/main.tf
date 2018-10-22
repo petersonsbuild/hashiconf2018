216aeee8a9bfd9433267c800dfc411adefe5f860
CCO-PAC\Ben.Peterson <ben.peterson@pacificlife.com>
2018-10-22 10:36:28 -0700

    added use-state project 2

--- use-state/main.tf
+++ use-state/main.tf
@@ -2,9 +2,16 @@ provider "aws" {
 version = "~> 1.16"
 }
 terraform {
-backend "s3" {
-    bucket = "pl-cld-benpeterson-engineering-prod-tfstate"
-    key = "use-state.tfstate"
-    encrypt = true
+    backend "s3" {
+        bucket = "pl-cld-benpeterson-engineering-prod-tfstate"
+        key = "use-state.tfstate"
+        encrypt = true
+    }
 }
+data "terraform_remote_state" "root" {
+    backend = "s3"
+    config {
+        bucket = "pl-cld-benpeterson-engineering-prod-tfstate"
+        key = "root.tfstate"
+    }
 }
